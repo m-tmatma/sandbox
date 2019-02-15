d7b68a286b7cc79224ab71abd495339e03d84759
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-02-16 07:27:08 +0900

    ファイルリストを保存する

--- find-exe.py
+++ find-exe.py
@@ -1,8 +1,15 @@
 import os
 
+exeList = []
+
 for root, dirs, files in os.walk('C:\Program Files (x86)'):
 	for file in files:
 		base, ext = os.path.splitext(file)
 		if ext == '.exe':
 			fullPath = os.path.join(root, file)
 			print (fullPath)
+			exeList.append(fullPath)
+
+with open('files.txt', 'w') as fout:
+	for fullPath in exeList:
+		fout.write(fullPath + '\n')
